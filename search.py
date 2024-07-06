3e705ca11e127f918e31770ae3003a6eade49259
unhurried <unhurried@users.noreply.github.com>
2024-07-06 11:55:34 +0900

    Fixed keyword search function.

--- search.py
+++ search.py
@@ -1,13 +1,16 @@
 import os
 from pathlib import Path
+from typing import Literal
 from system import get_config_dir
 
 
-def search(note_dir: Path, key: str, non_recursive: bool, keyword: str = ""):
+def search(
+    note_dir: Path, key: Literal["list", "find"], non_recursive: bool, keyword: str = ""
+):
     config_dir = get_config_dir()
     search_file = config_dir.joinpath(f"{key}.search-ms")
 
-    search_template = config_dir.joinpath(f"resource/template.search-ms")
+    search_template = config_dir.joinpath(f"resource/{key}.search-ms.template")
     with open(search_template, "r", encoding="utf_8") as t, open(
         search_file, "w", encoding="utf_8"
     ) as f:
